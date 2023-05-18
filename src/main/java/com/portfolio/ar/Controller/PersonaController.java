3aa3dc44d7502b9ed3f1c011c7aaa6754846fdcb
Ariel391 <ariel_ore@hotmail.com>
2023-05-18 00:15:49 -0300

    Se actualizo

--- src/main/java/com/portfolio/ar/Controller/PersonaController.java
+++ src/main/java/com/portfolio/ar/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,7 +56,7 @@ public class PersonaController {
         return new ResponseEntity(new Mensaje("Persona eliminada"), HttpStatus.OK);
     } */
 
-    /*
+    
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody PersonaDto dtopersona) {
         if (StringUtils.isBlank(dtopersona.getNombre())) {
@@ -72,7 +73,7 @@ public class PersonaController {
         personaService.save(persona);
         return new ResponseEntity(new Mensaje("Persona creada"), HttpStatus.OK);
 
-    } */
+    } 
 
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody PersonaDto dtopersona) {
